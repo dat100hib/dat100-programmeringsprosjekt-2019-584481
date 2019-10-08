8f52cc187ba24bce412ecf51b4c0f7c75a5ed56c
581257 <54100236+581257@users.noreply.github.com>
2019-10-08 15:15:10 +0200

    opg3a

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -24,12 +24,16 @@ public class GPSUtils {
 
 	public static double findMin(double[] da) {
 
-		double min;
-
-		// TODO - START
-
-		throw new UnsupportedOperationException(TODO.method());
-
+		double min;		
+		min = da[0];
+		
+		for (double d : da) {
+			if (d < min) {
+				min = d;
+			}
+		}
+		
+		return min;
 		// TODO - SLUT
 
 	}
