160e9cd5ade2ae485c5a3ae2c3db0739f6a0d5d4
581257 <54100236+581257@users.noreply.github.com>
2019-10-09 11:03:13 +0200

    opg3d

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -55,18 +55,23 @@ public class GPSUtils {
 		return tab;
 	}
 
+	
 	private static int R = 6371000; // jordens radius
 	public static double distance(GPSPoint gpspoint1, GPSPoint gpspoint2) {
-
+		
 		double d;
 		double latitude1, longitude1, latitude2, longitude2;
-
-		// TODO - START
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO - SLUTT
-
+		//Radians = Degrees * PI / 180
+		double rlatitude1 = gpspoint1.getLatitude()*PI/180;
+		double rlatitude2 = gpspoint2.getLatitude()*PI/180;		
+		double dlatitude = rlatitude2-rlatitude1;
+		double dlongitude = gpspoint2.getLongitude()-gpspoint1.getLongitude();
+		double rlongitude = dlongitude *PI/180;
+		double a = pow(sin(dlatitude/2),2) + cos(rlatitude1)*cos(rlatitude2)*pow(sin(rlongitude/2),2);
+		double c = 2*atan2(sqrt(a),sqrt(1-a));
+		d = R*c;
+		return d;
+		
 	}
 
 	public static double speed(GPSPoint gpspoint1, GPSPoint gpspoint2) {
@@ -74,8 +79,7 @@ public class GPSUtils {
 		int secs;
 		double speed;
 
-		// TODO - START
-
+		
 		throw new UnsupportedOperationException(TODO.method());
 
 		// TODO - SLUTT
